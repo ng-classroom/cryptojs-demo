471ca828db515cd48b8c4a8ccc06188159c36226
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2019-06-23 11:50:31 -0500

    demo

---
+++ cryptojsdemo/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent {
+  title = 'EncryptionDecryptionSample';
+
+  plainText: string;
+  encryptText: string;
+  encPassword: string;
+  decPassword: string;
+  conversionEncryptOutput: string;
+  conversionDecryptOutput: string;
+
+  constructor() {
+  }
+
+  convertText(conversion: string) {
+      if (conversion === 'encrypt') {
+        this.conversionEncryptOutput = CryptoJS.AES.encrypt(this.plainText.trim(), this.encPassword.trim()).toString();
+      } else {
+        this.conversionDecryptOutput = CryptoJS.AES.decrypt(this.encryptText.trim(), this.decPassword.trim()).toString(CryptoJS.enc.Utf8);
+      }
+  }
+}
